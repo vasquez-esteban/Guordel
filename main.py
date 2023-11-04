cd1650549cfe4d494e3f5c30323247d71d672e61
vasquez-esteban <evasquezp@unal.edu.co>
2023-11-04 10:39:49 -0500

    Versión 0.1 - Se agrega seleccionador de palabras y selección predeterminada

---
+++ main.py
@@ -0,0 +1,9 @@
+# Requerimientos:
+
+# Tener un lemario
+# 4-8 Palabras
+# Se pueden jugar varias partidas
+# La interfaz debe mostrar la cantidad de aciertos y fallos cada partida
+# Cada partida termina si los intentos son iguales a seis y no se adivina o si se adivina
+# Abre el archivo de origen en modo lectura
+# Abre el archivo de texto en modo lectura
